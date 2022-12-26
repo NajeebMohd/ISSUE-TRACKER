35b792bf2b9e3fbfe2b5f72a6c9ba9a3e61e0e43
najeeb <mohdnajeeb9642@gmail.com>
2022-12-26 15:50:24 +0530

    done ready to submit

--- controller/issue_controller.js
+++ controller/issue_controller.js
@@ -1,33 +1,6 @@
 const Issue = require('../models/issue');
 const Project = require('../models/project');
 
-// module.exports.CreateIssue = function(req,res){    
-//     Project.findOne({_id : req.body.projectid},function(err,projectData){        
-//         if(projectData){
-//             Issue.findOne({title : req.body.title},function(err,issuedata){
-//                 if(!issuedata){
-//                     Issue.create({
-    
-//                         title : req.body.title,
-//                         author : req.body.author,
-//                         discription : req.body.discription,
-//                         user : req.user._id,
-//                         project : req.body.projectid
-                        
-//                     },function(err,issue){
-//                         projectData.issues.push(issue);
-//                         projectData.save();
-//                         if(err){console.log('error while creating issue -->> ',err);return;}
-//                         return res.redirect('back');
-//                     });
-//                 }
-//             });
-//         }
-//         return res.redirect('back');               
-//     });    
-// }
-
-
 module.exports.CreateIssue = function(req,res){    
     Project.findOne({_id : req.body.projectid},function(err,projectData){        
         if(projectData){
--- models/issue.js
+++ models/issue.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const issueSchema = new mongoose.Schema({
     title:{
         type : String,
-        required : true,        
+        required : true,
+        unique : false        
     },
     author : {
         type : String,
