10297652d11812b83142f96c2706a16594a87081
najeeb <mohdnajeeb9642@gmail.com>
2023-01-11 15:10:20 +0530

    fix in mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/Issue_Tracker');
+mongoose.connect('mongodb://127.0.0.1:27017/Issue_Tracker');
 
 const db = mongoose.connection;
 
